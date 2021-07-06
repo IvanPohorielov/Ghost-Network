8776f90b512232e3c3e39cde4e2caceaea498c9b
Ivan Pohorielov <rideonkawasaki@gmail.com>
2021-07-06 10:08:37 +0300

    Initial Commit

---
+++ Ghost Network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ghost Network
+//
+//  Created by MacBook on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
